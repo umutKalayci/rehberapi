a408fc655df9ea32d6c2faf2ba25d38c62a20c61
Melike <tiryakimelike26@gmail.com>
2023-07-06 17:21:57 +0300

    database fixed.

--- ApiProject/ApiApplication/serializers.py
+++ ApiProject/ApiApplication/serializers.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
 from .models import *
 class SirketSerializer(serializers.Serializer):
-    sirket_id= serializers.IntegerField(label="enter sirket id")
+    #sirket_id= serializers.IntegerField(label="enter sirket id")
     sirket_isim = serializers.CharField(label="enter sirket isim")
     sirket_adres = serializers.CharField(label="enter sirket adres")
     sirket_tel = serializers.CharField(label="enter sirket tel")
     sirket_email = serializers.EmailField(label="enter sirket email")
     sirket_web = serializers.URLField(label="enter sirket web")
 
+    def create(self, validated_data):
+        sirket = Sirket.objects.create(**validated_data)
+        return sirket
+
 class KisiSerializer(serializers.Serializer):
-    kisi_id= serializers.IntegerField(label="enter kisi id")
+    #kisi_id= serializers.IntegerField(label="enter kisi id")
     kisi_adsoyad = serializers.CharField(label="enter kisi adsoyad")
     kisi_tel = serializers.CharField(label="enter kisi tel")
     kisi_email = serializers.EmailField(label="enter kisi email")
-    kisi_sirket = serializers.IntegerField(label="Şirket ID")   
+    kisi_sirket_id = serializers.PrimaryKeyRelatedField(queryset=Sirket.objects.all(), label="Şirket ID")
+
+    def create(self, validated_data):
+        kisi_sirket_id = validated_data.pop('kisi_sirket_id').sirket_id
+        kisi = Kisi.objects.create(kisi_sirket_id=kisi_sirket_id, **validated_data)
+        return kisi
--- ApiProject/ApiApplication/views.py
+++ ApiProject/ApiApplication/views.py
@@ -17,19 +17,12 @@ class SirketApiView(APIView):
         print('Request data is:',request.data)
         serializer_obj=SirketSerializer(data=request.data)
         if(serializer_obj.is_valid()):
+            sirket = serializer_obj.save()
+            sirket_data = serializer_obj.data
+            return Response({"Message": "Yeni sirket eklendi!", "Sirket": sirket_data})
 
-
-            Sirket.objects.create(
-                            sirket_id=serializer_obj.data.get("sirket_id"),
-                            sirket_isim=serializer_obj.data.get("sirket_isim"),
-                            sirket_adres=serializer_obj.data.get("sirket_adres"),
-                            sirket_tel=serializer_obj.data.get("sirket_tel"),
-                            sirket_email=serializer_obj.data.get("sirket_email"),
-                            sirket_web=serializer_obj.data.get("sirket_web"),
-                            )
-        
-        sirket=Sirket.objects.all().filter(sirket_id=request.data["sirket_id"]).values()
-        return Response({"Message":"Yeni sirket eklendi!","Sirket":sirket})
+        return Response(serializer_obj.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 class KisiApiView(APIView):
     serializer_class=KisiSerializer
@@ -43,26 +36,11 @@ class KisiApiView(APIView):
         print('Request data is:',request.data)
         serializer_obj=KisiSerializer(data=request.data)
         if(serializer_obj.is_valid()):
-                kisi_id = serializer_obj.data.get("kisi_id")
-                kisi_adsoyad = serializer_obj.data.get("kisi_adsoyad")
-                kisi_tel = serializer_obj.data.get("kisi_tel")
-                kisi_email = serializer_obj.data.get("kisi_email")
-                sirket_id = serializer_obj.data.get("sirket_id")
+                kisi = serializer_obj.save()
+                kisi_data = serializer_obj.data
+                return Response({"Message": "Yeni kisi eklendi!", "Kisi": kisi_data})
 
-                if "sirket_id" in serializer_obj.data:
-                    sirket = Sirket.objects.get(sirket_id=sirket_id)
-                    Kisi.objects.create(
-                        kisi_id=kisi_id,
-                        kisi_adsoyad=kisi_adsoyad,
-                        kisi_tel=kisi_tel,
-                        kisi_email=kisi_email,
-                        sirket=sirket
-                    )
-
-                kisi = Kisi.objects.all().filter(kisi_id=kisi_id).values()
-                return Response({"Message": "Yeni kisi eklendi!", "Kisi": kisi})
-        else:
-            return Response(serializer_obj.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer_obj.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
